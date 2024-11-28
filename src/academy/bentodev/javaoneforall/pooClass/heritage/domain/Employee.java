c97c2b2d740f482ef8caf4f4cd6c554e0a43a676
Lucas-Bento-D <lucasbentoskl@outlook.com>
2024-11-28 13:41:42 -0300

    Feat: Heritage pt2 - super method

--- src/academy/bentodev/javaoneforall/pooClass/heritage/domain/Employee.java
+++ src/academy/bentodev/javaoneforall/pooClass/heritage/domain/Employee.java
@@ -8,6 +8,17 @@ package academy.bentodev.javaoneforall.pooClass.heritage.domain;
 public class Employee extends Person{
     private double salary;
 
+    // aqui estamos sobreescrevendo o metodo print de Person
+    // ou seja, quando um 'Employee' chamar o metodo print()
+    // vai ser esse e não o print() de 'Person'
+    public void print(){
+        // super é uma referencia ao objeto pai de 'Employee'
+        // que é o 'person', sendo assim, nós podemos chamar a função print
+        // de person e depois executar algo no print de 'Employee'
+        super.print();
+        System.out.println(this.salary);
+    }
+
     public double getSalary() {
         return salary;
     }
