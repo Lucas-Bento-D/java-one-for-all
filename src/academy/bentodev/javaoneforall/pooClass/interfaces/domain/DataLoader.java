2b823e45c45a70a2dd93cbd749b674450b901511
Lucas-Bento-D <lucasbentoskl@outlook.com>
2024-12-07 14:11:17 -0300

    Feat: interfaces pt3 - atributes and static methods

--- src/academy/bentodev/javaoneforall/pooClass/interfaces/domain/DataLoader.java
+++ src/academy/bentodev/javaoneforall/pooClass/interfaces/domain/DataLoader.java
@@ -2,8 +2,13 @@ package academy.bentodev.javaoneforall.pooClass.interfaces.domain;
 
 /**
  * Em interfaces todos os métodos são publics e abstracts, entretanto podemos também construir métodos concretos
+ * Interfaces não podem ser instanciadas
  */
 public interface DataLoader {
+    /**
+     * Em uma interface, todos seus atributos são constantes
+     */
+    int MAX_DATA_SIZE = 10;
     void load();
 
     /**
@@ -11,5 +16,12 @@ public interface DataLoader {
      */
     default void checkPermission(){
         System.out.println("checando permissão");
+    }
+
+    /**
+     * podemos fazer funções estaticas nas interfaces
+     */
+    public static void retriveMaxDataSize(){
+
     }
 }
