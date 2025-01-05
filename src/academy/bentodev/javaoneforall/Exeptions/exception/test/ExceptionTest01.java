7e99b4f182b2b14431eff74509be8f56c6105d04
Lucas-Bento-D <lucasbentoskl@outlook.com>
2025-01-05 20:41:46 -0300

    exceptions pt5 - document checked exception

--- src/academy/bentodev/javaoneforall/Exeptions/exception/test/ExceptionTest01.java
+++ src/academy/bentodev/javaoneforall/Exeptions/exception/test/ExceptionTest01.java
@@ -7,6 +7,13 @@ public class ExceptionTest01 {
     public static void main(String[] args) {
         createNewFile();
     }
+
+    /**
+     * Sobre o tratamento de try/catch, nós podemos jogar a responsabilidade para quem estiver utilizando essa função
+     * para isso podemos ussar "throws IOException" por exemplo para especificar que a função poderá retornar um erro.
+     * Trazendo isso pro nosso uso atual, poderiamos utilizar o termo para createNewFile e a função main iria precisar
+     * tratar caso desse erro, ou seja, o tratamento seria responsabilidade de main.
+     */
     private static void createNewFile(){
         File file = new File("archive/test.txt");
         try{
