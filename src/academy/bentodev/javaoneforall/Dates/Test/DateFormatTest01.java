76ab7d6d5ee9454163f7b7874e7440aafa7e2f07
Lucas-Bento-D <lucasbentoskl@outlook.com>
2025-01-21 14:57:12 -0300

    feat: utilitary classes - DateFormate

---
+++ src/academy/bentodev/javaoneforall/Dates/Test/DateFormatTest01.java
@@ -0,0 +1,22 @@
+package academy.bentodev.javaoneforall.Dates.Test;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class DateFormatTest01 {
+    public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
+        DateFormat[] formatedDate = new DateFormat[7];
+        formatedDate[0] = DateFormat.getInstance();
+        formatedDate[1] = DateFormat.getDateInstance();
+        formatedDate[2] = DateFormat.getDateTimeInstance();
+        formatedDate[3] = DateFormat.getDateInstance(DateFormat.SHORT);
+        formatedDate[4] = DateFormat.getDateInstance(DateFormat.MEDIUM);
+        formatedDate[5] = DateFormat.getDateInstance(DateFormat.LONG);
+        formatedDate[6] = DateFormat.getDateInstance(DateFormat.FULL);
+
+        for (DateFormat fd: formatedDate){
+            System.out.println(fd.format(calendar.getTime()));
+        }
+    }
+}
