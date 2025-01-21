fec976096dae15fc1a490758be087168f141ea4a
Lucas-Bento-D <lucasbentoskl@outlook.com>
2025-01-21 15:18:33 -0300

    feat: utilitary classes - dates internacionalization with Locale

---
+++ src/academy/bentodev/javaoneforall/Dates/Test/LocaleTest01.java
@@ -0,0 +1,31 @@
+package academy.bentodev.javaoneforall.Dates.Test;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class LocaleTest01 {
+    public static void main(String[] args) {
+        Locale localeItaly = new Locale("it", "IT");
+        Locale localeCH = new Locale("it", "CH");
+        Locale localeHindi = new Locale("hi", "IN");
+        Locale localeJapan = new Locale("ja", "JP");
+        Locale localeNerdlands = new Locale("nl", "NL");
+
+        Calendar calendar = Calendar.getInstance();
+        DateFormat df1 = DateFormat.getDateInstance(DateFormat.FULL, localeItaly);
+        DateFormat df2 = DateFormat.getDateInstance(DateFormat.FULL, localeCH);
+        DateFormat df3 = DateFormat.getDateInstance(DateFormat.FULL, localeHindi);
+        DateFormat df4 = DateFormat.getDateInstance(DateFormat.FULL, localeJapan);
+        DateFormat df5 = DateFormat.getDateInstance(DateFormat.FULL, localeNerdlands);
+
+
+        System.out.println(df1.format(calendar.getTime()));
+        System.out.println(df2.format(calendar.getTime()));
+        System.out.println(df3.format(calendar.getTime()));
+        System.out.println(df4.format(calendar.getTime()));
+        System.out.println(df5.format(calendar.getTime()));
+        System.out.println(localeItaly.getDisplayCountry());
+        System.out.println(localeJapan.getDisplayCountry());
+    }
+}
