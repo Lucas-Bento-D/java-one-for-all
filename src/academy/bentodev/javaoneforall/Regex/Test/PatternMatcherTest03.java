ebbb29c139159e8fdac4f284d91177507bb43dc6
Lucas-Bento-D <lucasbentoskl@outlook.com>
2025-01-24 01:44:53 -0300

    feat: utilitary classes - regex pt02

--- src/academy/bentodev/javaoneforall/Regex/Test/PatternMatcherTest03.java
+++ src/academy/bentodev/javaoneforall/Regex/Test/PatternMatcherTest03.java
@@ -1,10 +1,9 @@
 package academy.bentodev.javaoneforall.Regex.Test;
 
-import javax.sound.sampled.FloatControl;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PatternMatcherTest02 {
+public class PatternMatcherTest03 {
     public static void main(String[] args) {
 
         /**
@@ -16,11 +15,19 @@ public class PatternMatcherTest02 {
          * \w -> caracteres de a-z, ou seja, todas letras do alfabeto, digitos e underline(basicamente tudo menos os caracteres especiais)
          * \W -> tudo que nao for incluso no \w
          * [] -> range de coisas que vc escolhe colocar
+         * ? -> 0 ou 1
+         * * -> 0 ou mais
+         * + -> uma ou mais ocorrencias
+         * {n,m} -> de n a m
+         * () -> agrupamento
+         * | -> ou
+         * $ -> fim da linha
+         * . -> caracter coringa
          */
 
-        String regex = "0[xX][0-9a-fA-F]";
+        String regex = "0[xX]([0-9a-fA-F])+(\\s|$)";
 
-        String textTest2 = "12 0x 0X 0xffabc 0x109 0x1";
+        String textTest2 = "12 0x 0X 0xffabc 0x10G 0x1";
 
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(textTest2);
--- src/academy/bentodev/javaoneforall/Regex/Test/PatternMatcherTest04.java
+++ src/academy/bentodev/javaoneforall/Regex/Test/PatternMatcherTest04.java
@@ -1,10 +1,9 @@
 package academy.bentodev.javaoneforall.Regex.Test;
 
-import javax.sound.sampled.FloatControl;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PatternMatcherTest02 {
+public class PatternMatcherTest04 {
     public static void main(String[] args) {
 
         /**
@@ -16,12 +15,21 @@ public class PatternMatcherTest02 {
          * \w -> caracteres de a-z, ou seja, todas letras do alfabeto, digitos e underline(basicamente tudo menos os caracteres especiais)
          * \W -> tudo que nao for incluso no \w
          * [] -> range de coisas que vc escolhe colocar
+         * ? -> 0 ou 1
+         * * -> 0 ou mais
+         * + -> uma ou mais ocorrencias
+         * {n,m} -> de n a m
+         * () -> agrupamento
+         * | -> ou
+         * $ -> fim da linha
+         * . -> caracter coringa
          */
 
-        String regex = "0[xX][0-9a-fA-F]";
+        String regex = "(\\S)+@([a-zA-Z])+(\\.([a-zA-Z])+){1,2}";
 
-        String textTest2 = "12 0x 0X 0xffabc 0x109 0x1";
-
+        String textTest2 = "luffy@hotmail.com, 123jotaro@gmail.com, !@$zoro@mail.com.br teste@gmail.com, sakura@mail, teste$!@mail.com";
+        System.out.println("Email valido");
+        System.out.println("!@$zoro@mail.com.br".matches(regex));
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(textTest2);
 
