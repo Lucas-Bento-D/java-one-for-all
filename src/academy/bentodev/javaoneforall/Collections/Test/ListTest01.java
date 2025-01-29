28cc424f78c7f88e7933e51fb3f75d52e151d3bc
Lucas-Bento-D <lucasbentoskl@outlook.com>
2025-01-28 22:48:24 -0300

    feat: utilitary classes - List pt2

--- src/academy/bentodev/javaoneforall/Collections/Test/ListTest01.java
+++ src/academy/bentodev/javaoneforall/Collections/Test/ListTest01.java
@@ -11,22 +11,38 @@ public class ListTest01 {
          * pois é isso que importa nesse momento, já no tempo da compilação real do código, o Java força para que
          * o tipo da Lista seja oque está dentro ta tag, ou seja, no bytecode o java entente List e no tempo de compilação real
          * o java força List<E>, sendo esse E um tipo inserido no código.
+         * Lembrando que esse tipo <E> precisa ser um Wrapper e nao um tipo primitivo, lembrando que Wrapper de tipos primitivos sao:
+         * Byte byteW = 1;
+         * Short shortW=1;
+         * Integer intW = 1;
+         * Long longW = 10L;
+         * Float floatW = 10F;
+         * Double doubleW = 10D;
+         * Character charW = 'W';
+         * Boolean booleanW = false;
          */
         List<String> names = new ArrayList<>();
+        List<String> names2 = new ArrayList<>();
         names.add("Willian");
         names.add("DevDojo");
+        names2.add("Suane");
+        names2.add("Academy");
+//        names.remove("Willian");
 
+        names.addAll(names2);
         for (String name: names){
             System.out.println(name);
         }
-        names.add("add novo");
         System.out.println("----------------");
         /**
          * Como estamos trabalhando com Lista(coleção) e não com arrays(sei que parece confuso), não pegamos o tamanho com
          * .length e sim com .size()
          */
-        for (int i = 0; i < names.size(); i++) {
+        int size = names.size();
+        for (int i = 0; i < size; i++) {
             System.out.println(names.get(i));
+            names.add("add novo"); // [e uma m[a pr[atica lidar com o tamanho da lista dentro de um for
         }
+        System.out.println(names);
     }
 }
